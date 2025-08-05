0e789ff444a894109808399a81a878405090d886
isaac451-andrade <166961938+isaac451-andrade@users.noreply.github.com>
2025-08-05 06:09:48 -0300

    deploy

--- back-end/apiSetup/wsgi.py
+++ back-end/apiSetup/wsgi.py
@@ -8,6 +8,15 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+# Adiciona o diretório do projeto ao sys.path
+# Este é um passo crucial para garantir que o Django encontre seus módulos.
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 from django.core.wsgi import get_wsgi_application
 
